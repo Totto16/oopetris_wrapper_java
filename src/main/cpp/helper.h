c157a901dd18c75b00decd8d4008d6284e40f9e5
Totto16 <tobiausgais@gmail.com>
2025-04-15 01:40:35 +0200

    feat: add Map implementation

--- src/main/cpp/helper.h
+++ src/main/cpp/helper.h
@@ -100,6 +100,7 @@
 
 #define JAVA_MAP_CLASS JAVA_UTIL_BASE_PACKAGE JAVA_CLASS_NAME_SEPERATOR "Map"
 
+#define JAVA_HASHMAP_CLASS JAVA_UTIL_BASE_PACKAGE JAVA_CLASS_NAME_SEPERATOR "HashMap"
 
 // functions
 
@@ -453,6 +454,42 @@ template<typename T>
 using JArrayList = JList<ArrayListImpl, T>;
 
 
+// map specific stuff
+
+
+template<typename T>
+concept IsJavaMapImpl = IsJavaTypeDescriptionForObject<T> && requires(T) {
+    { T::has_initial_capacity_constructor } -> std::convertible_to<bool>;
+};
+
+
+// see: https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/util/HashMap.html
+struct HashMapImpl {
+    static constexpr const char* java_class = JAVA_HASHMAP_CLASS;
+    static constexpr const char* java_type = TYPE_FOR_CLASS(JAVA_HASHMAP_CLASS);
+
+    static constexpr bool has_initial_capacity_constructor = true;
+
+    using native_type = jobject;
+};
+
+
+static_assert(IsJavaMapImpl<HashMapImpl>);
+
+
+struct MapConstructorEmpty {
+    using inner = std::tuple<>;
+};
+
+static_assert(IsJavaConstructor<MapConstructorEmpty>);
+
+
+struct MapConstructorWithCapacity {
+    using inner = std::tuple<JIntDescription>;
+};
+
+static_assert(IsJavaConstructor<MapConstructorWithCapacity>);
+
 struct JMapDescription {
     static constexpr const char* java_class = JAVA_MAP_CLASS;
     static constexpr const char* java_type = TYPE_FOR_CLASS(JAVA_MAP_CLASS);
@@ -461,3 +498,64 @@ struct JMapDescription {
 };
 
 static_assert(IsJavaTypeDescription<JMapDescription>);
+
+
+// see https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/util/Map.html
+template<typename MapImpl, typename Key, typename Value>
+    requires IsJavaMapImpl<MapImpl> && IsJavaTypeDescriptionForObject<Key> && IsJavaTypeDescriptionForObject<Value>
+class JMap {
+private:
+    jobject m_instance;
+    jclass m_class_impl;
+
+public:
+    explicit JMap(JNIEnv* env) {
+
+
+        const auto [map_class, map_instance] = construct_new_java_object_extended<MapImpl, MapConstructorEmpty>(env);
+
+        m_class_impl = map_class;
+        m_instance = map_instance;
+    }
+
+
+    explicit JMap(JNIEnv* env, jint initial_capacity)
+        requires MapImpl::has_initial_capacity_constructor
+    {
+
+        const auto [map_class, map_instance] =
+                construct_new_java_object_extended<MapImpl, MapConstructorWithCapacity>(env, initial_capacity);
+
+        m_class_impl = map_class;
+        m_instance = map_instance;
+    }
+
+    Value::native_type put(JNIEnv* env, Key::native_type key, Value::native_type value) {
+
+
+        const auto [_, map_put_function] = get_method_for_class(
+                env, m_class_impl, "put", method_type(Key::java_type + Value::java_type, Value::java_type)
+        );
+
+        static_assert(std::is_same_v<typename Value::native_type, jobject>);
+        jobject result = env->CallObjectMethod(m_instance, map_put_function, key, value);
+
+        if (result == nullptr) {
+            throw JavaException(RuntimeException, "Error in call to Map::put");
+        }
+
+        if (env->ExceptionOccurred() != nullptr) {
+            throw JavaExceptionAlreadyThrown();
+        }
+
+        return result;
+    }
+
+    [[nodiscard]] jobject get_result() const {
+        return m_instance;
+    }
+};
+
+template<typename Key, typename Value>
+    requires IsJavaTypeDescriptionForObject<Key> && IsJavaTypeDescriptionForObject<Value>
+using JHashMap = JMap<HashMapImpl, Key, Value>;
